dbdcde7dfd554ea25e93d9affe16e14cdcb015dc
Álvaro Condorí <condorialvaro95@gmail.com>
2025-03-28 18:32:26 -0300

    Hooks vs. Connect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import {Col} from "antd";
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
 import {getPokemon} from "./api";
 import logo from "./statics/logo.svg";
-import {setPokemons as setPokemonsActions} from "./actions";
+import {setPokemons} from "./actions";
 import "./App.css";
-import {connect} from "react-redux";
 
-function App({pokemons,setPokemons}) {
+function App() {
+  const pokemons = useSelector((state) => state.pokemons);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonRes = await getPokemon();
-      setPokemons(pokemonRes);
+      dispatch(setPokemons(pokemonRes));
     };
 
     fetchPokemons();
@@ -32,12 +34,4 @@ function App({pokemons,setPokemons}) {
   );
 }
 
-const mapStateToProps = (state) => ({
-  pokemons: state.pokemons,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setPokemons: (value) => dispatch(setPokemonsActions(value)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
